7f72d2bfceb52bbb9122808e70db4795ba4071f2
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2012-12-16 21:07:50 +0100

    Dollar side-effects? 2 - Test OK

--- TddByExampleMoney/src/main/java/es/duero4/tddbyexamplemoney/Dollar.java
+++ TddByExampleMoney/src/main/java/es/duero4/tddbyexamplemoney/Dollar.java
@@ -16,8 +16,8 @@ class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
     
 }
