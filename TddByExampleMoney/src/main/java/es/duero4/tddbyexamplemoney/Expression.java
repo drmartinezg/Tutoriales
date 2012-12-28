6f5f34fade768a960c79a926e4654bc9f7f817ea
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2012-12-28 23:57:01 +0100

    Chapter 15 - Make It : 5 - Polymorphism to elimate explicit class checking

--- TddByExampleMoney/src/main/java/es/duero4/tddbyexamplemoney/Expression.java
+++ TddByExampleMoney/src/main/java/es/duero4/tddbyexamplemoney/Expression.java
@@ -5,5 +5,5 @@ package es.duero4.tddbyexamplemoney;
  * @author Ramon
  */
 interface Expression {
-    
+    Money reduce(String to);
 }
--- TddByExampleMoney/src/main/java/es/duero4/tddbyexamplemoney/Money.java
+++ TddByExampleMoney/src/main/java/es/duero4/tddbyexamplemoney/Money.java
@@ -46,5 +46,10 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
 
+    @Override
+    public Money reduce(String to) {
+        return this;
+    }
+
     
 }
