56576612ce88b952b26bf6922a6ad216c5ec39c0
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-06-29 13:25:48 +0200

    Chapter 2 - Making the second test pass

--- TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/Template.java
+++ TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/Template.java
@@ -5,14 +5,16 @@ package es.duero4.templateengine;
  * @author Ramon
  */
 class Template {
+    private String variableValue;
 
     public Template(String templateText) {
     }
 
     void set(String variable, String value) {
+        this.variableValue = value;
     }
     
     public String evaluate() {
-        return "Hello, Reader";
+        return "Hello, " + variableValue;
     }
 }
--- TddForJavaDevelopers/TemplateEngine/src/test/java/es/duero4/templateengine/TestTemplate.java
+++ TddForJavaDevelopers/TemplateEngine/src/test/java/es/duero4/templateengine/TestTemplate.java
@@ -38,4 +38,11 @@ public class TestTemplate {
         template.set("name", "Reader");
         assertEquals("Hello, Reader", template.evaluate());
     }
+    
+    @Test
+    public void differentValue() throws Exception {
+        Template template = new Template("Hello, ${name}");
+        template.set("name", "someone else");
+        assertEquals("Hello, someone else", template.evaluate());
+    }
 }
