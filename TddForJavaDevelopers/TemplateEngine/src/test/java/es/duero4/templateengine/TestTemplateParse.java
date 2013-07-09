9e992a3fec6de4d9c1bf350808772efbc3304008
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-07-09 23:06:02 +0200

    Chapter 3 - Refactoring away duplication in our test method

--- TddForJavaDevelopers/TemplateEngine/src/test/java/es/duero4/templateengine/TestTemplateParse.java
+++ TddForJavaDevelopers/TemplateEngine/src/test/java/es/duero4/templateengine/TestTemplateParse.java
@@ -35,17 +35,19 @@ public class TestTemplateParse {
     
     @Test
     public void emptyTemplateRendersAsEmptyString() {
-        TemplateParse parse = new TemplateParse();
-        List<String> segments = parse.parse("");
+        List<String> segments = parse("");
         assertEquals("Number of segments", 1, segments.size());
         assertEquals("", segments.get(0));
     }
 
     @Test
     public void templateWithOnlyPlainText() {
-        TemplateParse parse = new TemplateParse();
-        List<String> segments = parse.parse("plain text only");
+        List<String> segments = parse("plain text only");
         assertEquals("Number of segments", 1, segments.size());
         assertEquals("plain text only", segments.get(0));
+    }
+
+    private List<String> parse(String template) {
+        return new TemplateParse().parse(template);
     }
 }
