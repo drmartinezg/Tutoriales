27ccbd24e81a4d258bd3f70190b491b618e2a2df
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-07-11 23:34:05 +0200

    Chapter 3 - Minimal implementations of the two Segment classes

--- TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/PlainText.java
+++ TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/PlainText.java
@@ -5,8 +5,15 @@ package es.duero4.templateengine;
  * @author Ramon
  */
 public class PlainText implements Segment {
+    private final String text;
 
     public PlainText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return text.equals(((PlainText) other).text);
     }
     
 }
--- TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/Variable.java
+++ TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/Variable.java
@@ -7,8 +7,14 @@ import es.duero4.templateengine.Segment;
  * @author Ramon
  */
 public class Variable implements Segment {
+    private final String name;
 
     public Variable(String name) {
+        this.name = name;
     }
     
+    @Override
+    public boolean equals(Object other) {
+        return name.equals(((Variable) other).name);
+    }
 }
