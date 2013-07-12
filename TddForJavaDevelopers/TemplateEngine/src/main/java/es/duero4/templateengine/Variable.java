1ea027aea69e5dc434720305bf3101b050858b67
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-07-12 18:58:32 +0200

    Chapter 3 - Checking for missing values in the Variable class at rendering time

--- TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/Variable.java
+++ TddForJavaDevelopers/TemplateEngine/src/main/java/es/duero4/templateengine/Variable.java
@@ -21,6 +21,10 @@ public class Variable implements Segment {
 
     @Override
     public String evaluate(Map<String, String> variables) {
+        if (!variables.containsKey(name)) {
+            throw new MissingValueException(
+                    "No value for ${" + name + "}");
+        }
         return variables.get(name);
     }
 }
