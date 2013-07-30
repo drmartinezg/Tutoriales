cd6631025a5c7ca5fde5b64f3408a71eaae150c7
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-07-30 17:57:40 +0200

    Chapter 5 - 5.4. Our first cut at the LoginServlet (reject any login attempt)

--- TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/web/controller/servlet/LoginServlet.java
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/web/controller/servlet/LoginServlet.java
@@ -1,6 +1,10 @@
 package es.duero4.tddinactionjee.web.controller.servlet;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -10,5 +14,13 @@ class LoginServlet extends HttpServlet {
 
     public LoginServlet() {
     }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Always reject login
+        response.sendRedirect("/invalidlogin");
+    }
+    
+    
     
 }
