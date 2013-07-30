70cac8f8fc6d046eb6924d71ebcef5024e1b0ddc
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-07-30 17:53:22 +0200

    Chapter 5 - 5.3. First test for the LoginServlet

---
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/web/controller/servlet/LoginServlet.java
@@ -0,0 +1,14 @@
+package es.duero4.tddinactionjee.web.controller.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author Ramon
+ */
+class LoginServlet extends HttpServlet {
+
+    public LoginServlet() {
+    }
+    
+}
---
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/web/controller/servlet/LoginServletTest.java
@@ -0,0 +1,51 @@
+package es.duero4.tddinactionjee.web.controller.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ramon
+ */
+public class LoginServletTest {
+    
+    public LoginServletTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void wrongPasswordShouldRedirectToErrorPage() throws Exception {
+        HttpServlet servlet = new LoginServlet();
+        // Create fake request object ...
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/login");
+        request.addParameter("j_username", "nosuchuser");
+        request.addParameter("j_password", "wrongpassword");
+        // ... and fake response object
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        servlet.service(request, response);
+        assertEquals("/invalidlogin", response.getRedirectedUrl());
+    }
+}
