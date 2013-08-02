d9a85c46b29e804ea9343c93615782b612003179
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-08-02 17:30:06 +0200

    Chapter 5 - 5.14. Testing a valid login

--- TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/web/controller/spring/LoginControllerTest.java
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/web/controller/spring/LoginControllerTest.java
@@ -1,5 +1,6 @@
 package es.duero4.tddinactionjee.web.controller.spring;
 
+import es.duero4.tddinactionjee.web.controller.authenticator.FakeAuthenticationService;
 import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.junit.After;
@@ -16,6 +17,8 @@ import static org.junit.Assert.*;
  * @author Ramon
  */
 public class LoginControllerTest {
+    private static final String CORRECT_PASSWORD = "correctpassword";
+    private static final String VALID_USERNAME = "validuser";
     
     public LoginControllerTest() {
     }
@@ -50,6 +53,24 @@ public class LoginControllerTest {
         
         // 3 - User should land on "wrong password" page
         assertEquals("wrongpassword", v.getViewName());
+    }
+    
+    @Test
+    public void validLoginFrowardsToFrontPage() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("GET");
+        request.addParameter("j_username", VALID_USERNAME);
+        request.addParameter("j_password", CORRECT_PASSWORD);
+        MockHttpServletResponse response = new MockHttpServletResponse();
         
+        FakeAuthenticationService mock = new FakeAuthenticationService();
+        mock.addUser(VALID_USERNAME, CORRECT_PASSWORD);
+        
+        LoginController c = new LoginController();
+        // Simulate dependency injection
+        c.setAuthenticationService(mock);
+        ModelAndView v = c.handleRequest(request, response);
+        
+        assertEquals("frontpage", v.getViewName());
     }
 }
