aa9ff919c18bd1be83553a57621938a690746e81
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-08-02 17:42:01 +0200

    Chapter 5 - 5.15. LoginController implementation, complete with dependency injection

--- TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/web/controller/spring/LoginController.java
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/web/controller/spring/LoginController.java
@@ -1,5 +1,6 @@
 package es.duero4.tddinactionjee.web.controller.spring;
 
+import es.duero4.tddinactionjee.web.controller.authenticator.AuthenticationService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,12 +11,22 @@ import org.springframework.web.servlet.mvc.Controller;
  * @author Ramon
  */
 class LoginController implements Controller {
+    private AuthenticationService authenticator;
 
     public LoginController() {
     }
 
-    public ModelAndView handleRequest(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
-        return new ModelAndView("wrongpassword");
+    void setAuthenticationService(AuthenticationService authService) {
+        this.authenticator = authService;
     }
     
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String user = request.getParameter("j_username");
+        String pass = request.getParameter("j_password");
+        if (authenticator.isValidLogin(user, pass)) {
+            return new ModelAndView("frontpage");
+        }
+        return new ModelAndView("wrongpassword");
+    }
+
 }
