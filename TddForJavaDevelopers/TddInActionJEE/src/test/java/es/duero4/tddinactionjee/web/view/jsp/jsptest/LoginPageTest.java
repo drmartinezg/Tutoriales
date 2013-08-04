a1307e9d2dd55686d5f97b6b423eeb3d7917cc50
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-08-04 20:57:08 +0200

    Chapter 5 - 5.19. Test for retaining the username but not the password upon a failed login

--- TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/web/view/jsp/jsptest/LoginPageTest.java
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/web/view/jsp/jsptest/LoginPageTest.java
@@ -12,4 +12,10 @@ public class LoginPageTest extends JspTestCase {
         form().shouldHaveField("j_password");
         form().shouldHaveSubmitButton("login");
     }
+    
+    public void testPreviousUsernameIsRetained() throws Exception {
+        setRequestAttribute("j_username", "bob");
+        get("/login.jsp");
+        form().field("j_username").shouldHaveValue("bob");
+    }
 }
