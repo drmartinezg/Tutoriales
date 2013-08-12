320926869a9a32d7d6e4710c97096abbb9da7c88
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-08-12 13:59:17 +0200

    Chapter 5 - 5.31. Adding components to our home page

--- TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/wicket/normal/MyHomePage.java
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/wicket/normal/MyHomePage.java
@@ -1,11 +1,18 @@
 package es.duero4.tddinactionjee.wicket.normal;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.PageLink;
 
 /**
  *
  * @author Ramon
  */
 public class MyHomePage extends WebPage {
+
+    public MyHomePage() {
+        add(new Label("welcomeMessage", "Welcome to the home page!"));
+        add(new PageLink("linkToPage", LoginPage.class ));
+    }
     
 }
