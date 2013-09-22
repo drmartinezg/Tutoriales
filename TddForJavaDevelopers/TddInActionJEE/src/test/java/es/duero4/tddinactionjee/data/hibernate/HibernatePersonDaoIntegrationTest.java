f27a8777120867979e2a3731a3fb851740cae1f2
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-09-22 17:04:07 +0200

    Chapter 6 - 6.15. Configuring a SessionFactory for test

--- TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/data/hibernate/HibernatePersonDaoIntegrationTest.java
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/data/hibernate/HibernatePersonDaoIntegrationTest.java
@@ -1,5 +1,11 @@
 package es.duero4.tddinactionjee.data.hibernate;
 
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+import org.hibernate.SessionFactory;
 import es.duero4.tddinactionjee.data.person.Person;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -49,5 +55,43 @@ public class HibernatePersonDaoIntegrationTest {
         Session s = sf.openSession();
         Object copy = s.get(Person.class, person.getId());
         assertEquals(person, copy);
+    }
+
+    private SessionFactory getSessionFactory() throws Exception {
+        return createConfiguration().buildSessionFactory();
+    }
+
+    private Configuration createConfiguration() throws Excepton {
+        // 1 - Load production config from classpath.
+        Configuration cfg = loadProductionConfiguration();
+        // 2 - Load test config from file
+        loadTestConfigInto(cfg, "/hibernate.test.properties");
+        return cfg;
+    }
+
+    private Configuration loadProductionConfiguration() {
+        // 1 - Load production config from classpath.
+        return new Configuration().configure();
+    }
+
+    private void loadTestConfigInto(Configuration cfg, String path) throws Exception {
+        // 2 - Load test config from file
+        Properties properties = loadPropertiesFrom(path);
+        Enumeration keys = properties.keys();
+        while (keys.hasMoreElements()) {            
+            String key = (String) keys.nextElement();
+            String value = properties.getProperty(key);
+            // 3 - Override individual properties
+            cfg.setProperty(key, value);
+        }
+    }
+
+    private Properties loadPropertiesFrom(String path) throws Exception {
+        InputStream stream = getClass().getResourceAsStream(path);
+        assertNotNull("Resource not found: " + path, stream);
+        Properties props = new Properties();
+        props.load(stream);
+        stream.close();
+        return props;
     }
 }
