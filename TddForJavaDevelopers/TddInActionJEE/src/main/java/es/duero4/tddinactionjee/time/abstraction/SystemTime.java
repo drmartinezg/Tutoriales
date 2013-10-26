7fe316d327382e8e7b00c934b5f4371f05600e2b
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-10-26 21:40:51 +0200

    Chapter 7 - 7.3.1. Trivial solution delegating directly to System.currentTimeMillis()

---
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/time/abstraction/SystemTime.java
@@ -0,0 +1,12 @@
+package es.duero4.tddinactionjee.time.abstraction;
+
+/**
+ *
+ * @author Ramon
+ */
+public class SystemTime {
+
+    public static long asMillis() {
+        return System.currentTimeMillis();
+    }
+}
