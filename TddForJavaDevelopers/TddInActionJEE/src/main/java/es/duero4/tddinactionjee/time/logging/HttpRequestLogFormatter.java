6e049d0130ecd917c1a8697eded294f9804aa759
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-12-08 19:58:53 +0100

    Chapter 7 - 7.7. HTTP request log formatter implementation using SystemTime

--- TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/time/logging/HttpRequestLogFormatter.java
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/time/logging/HttpRequestLogFormatter.java
@@ -1,6 +1,8 @@
 package es.duero4.tddinactionjee.time.logging;
 
+import es.duero4.tddinactionjee.time.abstraction.SystemTime;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -8,10 +10,23 @@ import javax.servlet.http.HttpServletRequest;
  * @author Ramon
  */
 public class HttpRequestLogFormatter {
-    public static DateFormat dateFormat;
+    public static DateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z");
 
-    public String format(HttpServletRequest request, int httpStatus, int contentLength) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String format(HttpServletRequest request, int httpStatusCode, int contentLength) {
+        StringBuffer line = new StringBuffer();
+        line.append(request.getRemoteAddr());
+        line.append(" - ");
+        line.append(request.getRemoteUser());
+        line.append(" [");
+        // 1 - Obtain current date and time from SystemTime
+        line.append(dateFormat.format(SystemTime.asDate()));
+        
+        line.append("] \"").append(request.getMethod());
+        line.append(" ").append(request.getRequestURI());
+        line.append(" ").append(request.getProtocol());
+        line.append("\" ").append(httpStatusCode);
+        line.append(" ").append(contentLength);
+        return line.toString();
     }
     
 }
