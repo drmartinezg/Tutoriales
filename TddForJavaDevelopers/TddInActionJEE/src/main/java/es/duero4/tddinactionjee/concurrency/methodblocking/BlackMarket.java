69e241a59964e9fabdaced2e1e3fb03fbee83004
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-12-14 20:38:18 +0100

    Chapter 7 - 7.13.1. Adding implementation to BlackMarket class

--- TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/concurrency/methodblocking/BlackMarket.java
+++ TddForJavaDevelopers/TddInActionJEE/src/main/java/es/duero4/tddinactionjee/concurrency/methodblocking/BlackMarket.java
@@ -1,16 +1,22 @@
 package es.duero4.tddinactionjee.concurrency.methodblocking;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author Ramon
  */
 public class BlackMarket {
-
+    private Semaphore tickets = new Semaphore(0);
+    
     public BlackMarket() {
     }
 
-    void buyTicket() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    void buyTicket() throws InterruptedException {
+        tickets.acquire();
     }
-    
+
+    public void sellTicket() {
+        tickets.release();
+    }
 }
