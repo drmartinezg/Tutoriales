bf8de69ca5c4766e91f2bae017fb8b85742af929
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-12-14 19:16:34 +0100

    Chapter 7 - 7.11. Improved version of our thread-safety test

---
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/concurrency/threadsafety/SynchedThread.java
@@ -0,0 +1,14 @@
+package es.duero4.tddinactionjee.concurrency.threadsafety;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ *
+ * @author Ramon
+ */
+public class SynchedThread extends Thread {
+
+    public SynchedThread(Runnable runnable, CyclicBarrier entryBarrier, CyclicBarrier exitBarrier) {
+    }
+    
+}
--- TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/concurrency/threadsafety/ThreadSafetyTest.java
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/concurrency/threadsafety/ThreadSafetyTest.java
@@ -1,5 +1,6 @@
 package es.duero4.tddinactionjee.concurrency.threadsafety;
 
+import java.util.concurrent.CyclicBarrier;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -64,6 +65,38 @@ public class ThreadSafetyTest {
         
         // 2 - Wait for threads to finish and assert counter's value
         Thread.sleep(500);
+        assertEquals(numberOfThreads * incrementsPerThread, codeUnderTest.value());
+    }
+    
+    @Test
+    public void testForThreadSafetyUsingCyclicBarrierToMaximizeConcurrency() throws Exception {
+        final Counter codeUnderTest = new Counter();
+        final int numberOfThreads = 20;
+        final int incrementsPerThread = 1000;
+        
+        // 1 - Create barrier for all created threads plus courrent one.
+        CyclicBarrier entryBarrier = new CyclicBarrier(numberOfThreads + 1);
+        CyclicBarrier exitBarrier = new CyclicBarrier(numberOfThreads + 1);
+        
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < incrementsPerThread; i++) {
+                    codeUnderTest.increment();
+                }
+            }
+        };
+        
+        // 2 - Custom Thread implementation waits on barrier
+        for (int i = 0; i < numberOfThreads; i++) {
+            new SynchedThread(runnable, entryBarrier, exitBarrier).start();
+        }
+        
+        assertEquals(0, codeUnderTest.value());
+        // 3 - Threads begin executing logic
+        entryBarrier.await();
+        // 4 - Wait for all threads to finish
+        exitBarrier.await();
         assertEquals(numberOfThreads * incrementsPerThread, codeUnderTest.value());
     }
 }
