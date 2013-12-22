a249832424e9d7beec4a8db1d1ff11d6dfa84f66
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2013-12-22 19:26:35 +0100

    Chapter 8 - 8.3. How to verify that the view works?

---
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/swing/mvp/pv/GreenBarViewImpl.java
@@ -0,0 +1,32 @@
+package es.duero4.tddinactionjee.swing.mvp.pv;
+
+import es.duero4.tddinactionjee.swing.mvp.common.GreenBarModel;
+import es.duero4.tddinactionjee.swing.mvp.common.GreenBarView;
+import es.duero4.tddinactionjee.swing.mvp.common.RunButtonListener;
+import java.awt.Color;
+
+/**
+ *
+ * @author Ramon
+ */
+public class GreenBarViewImpl implements GreenBarView {
+
+    public GreenBarViewImpl() {
+    }
+
+    @Override
+    public void addRunButtonListener(RunButtonListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(GreenBarModel model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setBarColor(Color newColor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ TddForJavaDevelopers/TddInActionJEE/src/test/java/es/duero4/tddinactionjee/swing/mvp/pv/GreenBarViewTest.java
@@ -0,0 +1,65 @@
+package es.duero4.tddinactionjee.swing.mvp.pv;
+
+import es.duero4.tddinactionjee.swing.mvp.common.GreenBarView;
+import es.duero4.tddinactionjee.swing.mvp.common.RunButtonListener;
+import java.awt.Color;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ramon
+ */
+public class GreenBarViewTest {
+    
+    private GreenBarView view;
+    // 1 - Check for received events
+    private boolean listenerReceivedClick;
+    
+    public GreenBarViewTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        view = new GreenBarViewImpl();
+        view.addRunButtonListener(new RunButtonListener() {
+            @Override
+            public void onRunButtonClick() {
+                // 1 - Check for received events
+                listenerReceivedClick = true;
+            }
+        });
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void viewShouldDisplayTheBarInTheGivenColor() throws Exception {
+        // 2 - Simulate the presenter
+        view.setBarColor(Color.GREEN);
+        // How to verify that the bar really is green?
+    }
+    
+    @Test
+    public void viewShouldDelegateGesturesToListener() throws Exception {
+        // How to simulate a click on the "Run" button?
+        // How to know whether the button is there at all?
+        
+        // 1 - Check for received events
+        assertTrue(listenerReceivedClick);
+    }
+}
