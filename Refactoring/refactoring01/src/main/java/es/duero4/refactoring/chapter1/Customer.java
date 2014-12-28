cc7a2aa4da79e34a62900227329a8cc8bdd510d5
ramon <ramon@ubuntu>
2014-12-29 00:34:14 +0100

    Refactoring - Chapter 1 - 08 - 2 - Replace temp with query [frequentRenterPoints by getTotalFrequentPoints()]

--- Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Customer.java
+++ Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Customer.java
@@ -25,23 +25,25 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
-
         String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            double amount = 0;
-            
-            frequentRenterPoints += rental.getFrequentRenterPoints();
-
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
         }
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentPoints()) + " frequent renter points";
 
         return result;
+    }
+
+    private int getTotalFrequentPoints() {
+        int total = 0;
+        for (Rental rental : rentals) {
+            total += rental.getFrequentRenterPoints();
+        }
+        return total;
     }
 
     private double getTotalCharge() {
