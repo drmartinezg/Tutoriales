08e73c4f69fef89d2ad3b2aee4182dabdd7932ed
ramon <ramon@ubuntu>
2014-12-28 19:40:42 +0100

    Refactoring - Chapter 1 - 01 - Orginal implementation.

---
+++ Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Rental.java
@@ -0,0 +1,26 @@
+package es.duero4.refactoring.chapter1;
+
+/**
+ *
+ * @author ramon
+ */
+class Rental {
+
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    
+}
