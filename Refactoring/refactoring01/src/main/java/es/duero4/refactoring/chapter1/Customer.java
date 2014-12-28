f99aee30d1880cdd34815ef6e4ef47ee4f8e2eb3
ramon <ramon@ubuntu>
2014-12-28 23:31:10 +0100

    Refactoring - Chapter 1 - 05 - 1 -Moved the amount calculation to Rental class

--- Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Customer.java
+++ Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Customer.java
@@ -55,26 +55,6 @@ public class Customer {
     }
 
     private double amountFor(Rental rental) {
-        double result = 0;
-        
-        // determine amounts for each line
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2) {
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return rental.getCharge();
     }
 }
