038ba6f4010d20e19c7c375e781e27e038c604d8
ramon <ramon@ubuntu>
2014-12-30 23:04:03 +0100

    Refactoring - Chapter 1 - 10 - 6 - Replacing Conditional with Polymorphism. Move getCharge method to Price. Left the delegate method in Movie though.

--- Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Movie.java
+++ Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Movie.java
@@ -42,29 +42,11 @@ class Movie {
         }
     }
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+    public double getCharge(int daysRented) {
+        return price.getCharge(daysRented);
     }
 
-    int getFrequentRenterPoints(int daysRented) {
+    public int getFrequentRenterPoints(int daysRented) {
         if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
             return 2;
         }
--- Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Price.java
+++ Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Price.java
@@ -6,4 +6,26 @@ package es.duero4.refactoring.chapter1;
  */
 public abstract class Price {
     public abstract int getPriceCode();
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
 }
