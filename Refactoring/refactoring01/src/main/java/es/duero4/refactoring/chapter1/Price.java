d53ee1e9bdfda69d3386afcc9c4e4104d44874b0
ramon <ramon@ubuntu>
2014-12-30 23:21:14 +0100

    Refactoring - Chapter 1 - 10 - 7 - Replacing Conditional with Polymorphism. Replacing the Price getCharge with a version in the RegularPrice's overridden method.

--- Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Price.java
+++ Refactoring/refactoring01/src/main/java/es/duero4/refactoring/chapter1/Price.java
@@ -7,15 +7,9 @@ package es.duero4.refactoring.chapter1;
 public abstract class Price {
     public abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
+    public double getCharge(int daysRented) {
         double result = 0;
         switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
             case Movie.NEW_RELEASE:
                 result += daysRented * 3;
                 break;
