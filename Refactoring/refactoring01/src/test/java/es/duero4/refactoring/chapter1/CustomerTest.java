9df6b5ad5bd101a5254559315b7825dcdc736507
ramon <ramon@ubuntu>
2015-01-04 20:18:47 +0100

    Refactoring - Chapter 1 - 11 -  Test refactoring.

--- Refactoring/refactoring01/src/test/java/es/duero4/refactoring/chapter1/CustomerTest.java
+++ Refactoring/refactoring01/src/test/java/es/duero4/refactoring/chapter1/CustomerTest.java
@@ -23,37 +23,37 @@ public class CustomerTest {
     @Test
     public void basicChildrensRental() {
         customer.addRental(new Rental(THE_HULK, 2));
-        assertThat(customer.statement(), is("Rental Record for Ramón\n\tThe Hulk\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points"));
+        assertThat(customer.statement(), is(expectedMessageFor("The Hulk", 1.5, 1.5, 1)));
     }
 
     @Test
     public void shouldDiscountChildrensRentals() {
         customer.addRental(new Rental(THE_HULK, 4));
-        assertThat(customer.statement(), is("Rental Record for Ramón\n\tThe Hulk\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"));
+        assertThat(customer.statement(), is(expectedMessageFor("The Hulk", 3.0, 3.0, 1)));
     }
     
     @Test
     public void basicNewReleaseRental() {
         customer.addRental(new Rental(IRON_MAN, 1));
-        assertThat(customer.statement(), is("Rental Record for Ramón\n\tIron Man 4\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"));
+        assertThat(customer.statement(), is(expectedMessageFor("Iron Man 4", 3.0, 3.0, 1)));
     }
     
     @Test
     public void shouldNotDiscountNewReleaseRentalButBonusFrequentRenterPoints() {
         customer.addRental(new Rental(IRON_MAN, 4));
-        assertThat(customer.statement(), is("Rental Record for Ramón\n\tIron Man 4\t12.0\nAmount owed is 12.0\nYou earned 2 frequent renter points"));
+        assertThat(customer.statement(), is(expectedMessageFor("Iron Man 4", 12.0, 12.0, 2)));
     }
     
     @Test
     public void basicRegularRental() {
         customer.addRental(new Rental(SPIDER_MAN, 2));
-        assertThat(customer.statement(), is("Rental Record for Ramón\n\tSpiderman\t2.0\nAmount owed is 2.0\nYou earned 1 frequent renter points"));
+        assertThat(customer.statement(), is(expectedMessageFor("Spiderman", 2.0, 2.0, 1)));
     }
     
     @Test
     public void shouldDiscountRegularRental() {
         customer.addRental(new Rental(SPIDER_MAN, 4));
-        assertThat(customer.statement(), is("Rental Record for Ramón\n\tSpiderman\t5.0\nAmount owed is 5.0\nYou earned 1 frequent renter points"));
+        assertThat(customer.statement(), is(expectedMessageFor("Spiderman", 5.0, 5.0, 1)));
     }
     
     @Test
@@ -68,5 +68,9 @@ public class CustomerTest {
     public void basicHtmlChildrensRental() {
         customer.addRental(new Rental(THE_HULK, 2));
         assertThat(customer.htmlStatement(), is("<h1>Rental Record for <b>Ramón</b></h1>\n<p>The Hulk\t1.5</p>\n<p>Amount owed is <b>1.5</b></p>\n<p>You earned <b>1</b> frequent renter points</p>"));
+    }
+    
+    private String expectedMessageFor(String rental, double price, double total, int renterPointsEarned) {
+        return "Rental Record for Ramón\n\t" + rental + "\t" + price + "\nAmount owed is " + total + "\nYou earned " + renterPointsEarned + " frequent renter points";
     }
 }
