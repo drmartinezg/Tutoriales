5cf2bc1edeb25ffb7f361bd8042bd56b9b1c8177
ramon <ramon@ubuntu>
2015-01-24 21:48:45 +0100

    HeadFirst - 02 - Observer -  Build display elements

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/observer/weather/ForecastDisplay.java
@@ -0,0 +1,36 @@
+package es.duero4.headfirst.observer.weather;
+
+/**
+ *
+ * @author ramon
+ */
+public class ForecastDisplay implements Observer, DisplayElement {
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+    private Subject wheatherData;
+
+    public ForecastDisplay(Subject wheatherData) {
+        this.wheatherData = wheatherData;
+        wheatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        lastPressure = currentPressure;
+        currentPressure = pressure;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Forecast:");
+        if (currentPressure > lastPressure) {
+            System.out.println("Improving weather on the way");
+        } else if (currentPressure == lastPressure) {
+            System.out.println("More of the same");
+        } else if (currentPressure < lastPressure) {
+            System.out.println("Watch out for cooler, rainy weather");
+        }
+    }
+    
+}
--- Patterns/headfirst/src/main/java/es/duero4/headfirst/observer/weather/Observer.java
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/observer/weather/Observer.java
@@ -5,5 +5,5 @@ package es.duero4.headfirst.observer.weather;
  * @author ramon
  */
 public interface Observer {
-    public void update(float temp, float humidity, float pressure);
+    public void update(float temperature, float humidity, float pressure);
 }
