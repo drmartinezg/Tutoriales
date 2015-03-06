b8a10dca093b3b2d872f393b3106b3f007da72b9
ramon <ramon@ubuntu>
2015-03-06 21:39:07 +0100

    HeadFirst - 04 - Factory -  Franchising the pizza store. Modified main class

--- Patterns/headfirst/src/main/java/es/duero4/headfirst/factory/pizzafm/PizzaTestDrive.java
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/factory/pizzafm/PizzaTestDrive.java
@@ -14,24 +14,24 @@ public class PizzaTestDrive {
         System.out.println("Ethan ordered a " + pizza.getName() + "\n");
         
         pizza = chicagoStore.orderPizza("cheese");
-        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
         
         pizza = nyStore.orderPizza("clam");
         System.out.println("Ethan ordered a " + pizza.getName() + "\n");
         
         pizza = chicagoStore.orderPizza("clam");
-        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
         
         pizza = nyStore.orderPizza("pepperoni");
         System.out.println("Ethan ordered a " + pizza.getName() + "\n");
         
         pizza = chicagoStore.orderPizza("pepperoni");
-        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
         
         pizza = nyStore.orderPizza("veggie");
         System.out.println("Ethan ordered a " + pizza.getName() + "\n");
         
         pizza = chicagoStore.orderPizza("veggie");
-        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
     }
 }
