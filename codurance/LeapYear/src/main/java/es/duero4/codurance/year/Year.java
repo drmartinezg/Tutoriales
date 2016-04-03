b348ea8f413b258757f05b17592501cf6a292a49
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2016-04-03 23:18:41 +0200

    Leap Year Kata - 02 - Test not_be_a_leap_year_if not_divisible_by_4 fail

--- codurance/LeapYear/src/main/java/es/duero4/codurance/year/Year.java
+++ codurance/LeapYear/src/main/java/es/duero4/codurance/year/Year.java
@@ -1,5 +1,11 @@
 package es.duero4.codurance.year;
 
 public class Year {
+
+    public Year(int year) {
+    }
     
+    public Boolean leapYear() {
+        return null;
+    }
 }
--- codurance/LeapYear/src/test/java/es/duero4/codurance/year/YearShould.java
+++ codurance/LeapYear/src/test/java/es/duero4/codurance/year/YearShould.java
@@ -1,7 +1,15 @@
 package es.duero4.codurance.year;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class YearShould {
+  
+    @Test public void
+    not_be_a_leap_year_if_not_divisible_by_4() {
+        Year year = new Year(1997);
+        assertThat(year.leapYear(), is(false));
+    }
 }
