3e9a64f0401f90677f1f29e411d71f66d53e7d3c
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2016-04-03 23:41:16 +0200

    Leap Year Kata - 08 - Test not_be_a_leap_year_if_divisible_by_100_but_not_by_400 fail

--- codurance/LeapYear/src/test/java/es/duero4/codurance/year/YearShould.java
+++ codurance/LeapYear/src/test/java/es/duero4/codurance/year/YearShould.java
@@ -22,6 +22,11 @@ public class YearShould {
         assertThat(leapYear(1996), is(true));
     }
     
+    @Test public void
+    not_be_a_leap_year_if_divisible_by_100_but_not_by_400() {
+        assertThat(leapYear(1800), is(false));
+    }
+    
     private boolean leapYear(int year) {
         return new Year(year).leapYear();
     }
