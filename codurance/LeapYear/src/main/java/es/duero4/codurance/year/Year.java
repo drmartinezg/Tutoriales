4abfb70b8909f1c7adbda60a7a2542c123640d74
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2016-04-03 23:32:03 +0200

    Leap Year Kata - 03 - Test be_a_leap_year_if divisible_by_4 pass

--- codurance/LeapYear/src/main/java/es/duero4/codurance/year/Year.java
+++ codurance/LeapYear/src/main/java/es/duero4/codurance/year/Year.java
@@ -1,11 +1,13 @@
 package es.duero4.codurance.year;
 
 public class Year {
+    private final int year;
 
     public Year(int year) {
+        this.year = year;
     }
     
     public Boolean leapYear() {
-        return false;
+        return year % 4 == 0;
     }
 }
