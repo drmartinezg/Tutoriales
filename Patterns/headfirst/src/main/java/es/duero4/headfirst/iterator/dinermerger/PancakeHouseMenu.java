ea94876f09920c30d482cdaed3252154f7928137
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2016-12-28 22:31:16 +0100

    HeadFirst - 09 - Iterator - Menu implementations

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/iterator/dinermerger/PancakeHouseMenu.java
@@ -0,0 +1,46 @@
+package es.duero4.headfirst.iterator.dinermerger;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ramon
+ */
+public class PancakeHouseMenu {
+    private ArrayList menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList();
+        
+        addItem("k&B's Pancake Breakfast",
+                "Pancakes with scrambled eggs, and toast",
+                true, 2.99);
+        
+        addItem("Regular Pancake Breakfast", 
+                "Pancakes with fried eggs, sausage", 
+                false, 2.99);
+        
+        addItem("Blueberry Pancakes", 
+                "Pancakes made with fresh blueberries", 
+                true, 3.49);
+        
+        addItem("Waffles", 
+                "Waffles, with your choice of blueberries o strawberries", 
+                true, 3.59);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+    
+    public ArrayList getMenuItems() {
+        return menuItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Objectville Pancake House Menu";
+    }
+    
+}
