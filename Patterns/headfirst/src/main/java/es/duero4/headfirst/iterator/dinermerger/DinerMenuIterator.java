fa066a50361ee1868ef13230255cd3b2b96e044b
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2016-12-29 20:34:59 +0100

    HeadFirst - 09 - Iterator - Adding an Iterator to DinerMenu

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/iterator/dinermerger/DinerMenuIterator.java
@@ -0,0 +1,31 @@
+package es.duero4.headfirst.iterator.dinermerger;
+
+/**
+ *
+ * @author ramon
+ */
+public class DinerMenuIterator implements Iterator {
+    private MenuItem[] items;
+    private int position = 0;
+
+    public DinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (position >= items.length || items[position] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+        MenuItem menuItem = items[position];
+        position = position + 1;
+        return menuItem;
+    }
+    
+}
---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/iterator/dinermerger/Iterator.java
@@ -0,0 +1,10 @@
+package es.duero4.headfirst.iterator.dinermerger;
+
+/**
+ *
+ * @author ramon
+ */
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
