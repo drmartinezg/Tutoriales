9338b51d3d48f06364cbfdc5da786eafb0f34006
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2016-12-30 19:23:54 +0100

    HeadFirst - 09 - Iterator - Cleaning things up with java.util.Iterator

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/iterator/dinermergeri/MenuItem.java
@@ -0,0 +1,41 @@
+package es.duero4.headfirst.iterator.dinermergeri;
+
+/**
+ *
+ * @author ramon
+ */
+public class MenuItem {
+    private String name;
+    private String description;
+    private boolean vegetarian;
+    private double price;
+
+    public MenuItem(String name, String description, boolean vegetarian, double price) {
+        this.name = name;
+        this.description = description;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    @Override
+    public String toString() {
+        return (name + ", $" + price + "\n " + description);
+    }
+    
+}
