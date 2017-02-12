9b9b0249113a9732258fa154467121eb73db3bbe
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2017-02-12 16:44:35 +0100

    HeadFirst - 09 - Composite - Implementing the Composite Menu

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/composite/menu/Menu.java
@@ -0,0 +1,50 @@
+package es.duero4.headfirst.composite.menu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ramon
+ */
+public class Menu extends MenuComponent {
+    private ArrayList menuComponents = new ArrayList();
+    private String name;
+    private String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    
+    @Override
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+    
+    @Override
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+    
+    @Override
+    public MenuComponent getChild(int i) {
+        return (MenuComponent) menuComponents.get(i);
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public String getDescription() {
+        return description;
+    }
+    
+    @Override
+    public void print() {
+        System.out.println("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("---------------------");
+    }
+}
