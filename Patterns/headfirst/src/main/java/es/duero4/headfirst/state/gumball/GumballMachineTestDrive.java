6bfa1142c7c75303dac4cdfa695280edb752fc1f
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2017-10-14 22:49:56 +0200

    HeadFirst - 10 - State - State machines 101

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/state/gumball/GumballMachineTestDrive.java
@@ -0,0 +1,44 @@
+package es.duero4.headfirst.state.gumball;
+
+/**
+ *
+ * @author ramon
+ */
+public class GumballMachineTestDrive {
+
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+        
+        System.out.println(gumballMachine);
+        
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        
+        System.out.println(gumballMachine);
+        
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+        
+        System.out.println(gumballMachine);
+        
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+        
+        System.out.println(gumballMachine);
+        
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        
+        System.out.println(gumballMachine);
+    }
+    
+}
