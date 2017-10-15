7429019f7e5af46575e353354d5a0e83099ecf92
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2017-10-15 20:57:49 +0200

    HeadFirst - 10 - State - Adding Gumball Machine Test Class

---
+++ Patterns/headfirst/src/main/java/es/duero4/headfirst/state/gumballstate/GumballMachineTestDrive.java
@@ -0,0 +1,26 @@
+package es.duero4.headfirst.state.gumballstate;
+
+/**
+ *
+ * @author ramon
+ */
+public class GumballMachineTestDrive {
+
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+}
